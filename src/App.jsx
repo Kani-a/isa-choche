edf1e583d2c5ba33656ab866c06eb938279a54c1
kani-a <allanchagasilva@gmail.com>
2025-04-22 05:33:26 -0300

    color corection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import imagens from "./assets/imagens";
+import './style.css'
 
 import { DropdownButton } from "./componets/buttons/dropdown";
 
@@ -7,37 +8,55 @@ export default function App() {
   return  (
           <>
 
-<header className="header flex justify-center">
-        <section className="sidebar btn">
+<header className="header flex  fixed  w-full px-5 top-0 left-0 justify-between pink-banner">
+        <section className="sidebar">
           <nav>
-
-        <DropdownButton/>
-
+            <DropdownButton/>
           </nav>
         </section>
-        <h1 className="text-xl">
+        <h1 className="text-md w-full text-center">
           Isa choche e pá a logo e num sei oq
         </h1>
       </header>
 
       <main>
-        <section className="Banner flex justify-center bg-red-200 h-50 my-10">
-          bannere e num sei oq
+        <section className="pink-banner flex bg-red-200 h-25 my-10">
+          <p className="prod w-full flex justify-center  mx-10 ">
+            banner e num sei oq 
+          </p>
         </section>
-
-        <section className="main-prod bg-pink-200 flex justify-center">
+  
+        <section className="main-prod flex justify-center my-10">
 
           <ul className="prod-container grid grid-cols-2 md:grid-cols-4  gap-x-7 p-3 size-max">
-
-            <li className="prod">
+          <li className="m-1 prod">
               <img src={imagens[1].thumb} alt="imagen-prod" />
               <label htmlFor="imagen-prod" >
                 {imagens[1].titulo}
               </label>
             </li>
-
+            <li className="m-1 prod">
+              <img src={imagens[1].thumb} alt="imagen-prod" />
+              <label htmlFor="imagen-prod" >
+                {imagens[1].titulo}
+              </label>
+            </li>  <li className="m-1 prod">
+              <img src={imagens[1].thumb} alt="imagen-prod" />
+              <label htmlFor="imagen-prod" >
+                {imagens[1].titulo}
+              </label>
+            </li>  <li className="m-1 prod">
+              <img src={imagens[1].thumb} alt="imagen-prod" />
+              <label htmlFor="imagen-prod" >
+                {imagens[1].titulo}
+              </label>
+            </li>  
           </ul>
         </section>
-      </main>
-          </>)
+          
+        <br />
+
+        <p id="minhabunda">trepa trepa</p>
+              </main>
+</>)
 }
--- src/componets/buttons/dropdown.jsx
+++ src/componets/buttons/dropdown.jsx
@@ -1,15 +1,32 @@
-import { Dropdown, DropdownItem } from "flowbite-react";
+import { createTheme, Dropdown, DropdownItem, ThemeProvider } from "flowbite-react";
 
 export function DropdownButton() {
+  const customTheme = createTheme({
+    button:{
+      color:{
+        primary: "bg-red-700 hover:bg-red-600 w-sm",
+        secondary: "bg-blue-500 hover:bg-blue-600",
+      },
+      size:{
+        smal: "w-sm"
+      },
+      position:{
+        start : "left-0"
+      }
+    },
+  })
   return (
-    <Dropdown label="Dropdown button" dismissOnClick={false}>
-      <DropdownItem> 
-            Dashboard
-      </DropdownItem>
-      
-      <DropdownItem>Settings</DropdownItem>
-      <DropdownItem>Earnings</DropdownItem>
-      <DropdownItem>Sign out</DropdownItem>
-    </Dropdown>
+    <ThemeProvider theme={customTheme}>
+      <Dropdown  dismissOnClick={false} color="primary"  size="small">
+        <DropdownItem> 
+          <a href="#minhabunda">
+              Dashboard
+          </a>
+        </DropdownItem>
+        <DropdownItem>Settings</DropdownItem>
+        <DropdownItem>Earnings</DropdownItem>
+        <DropdownItem>Sign out</DropdownItem>
+      </Dropdown>
+    </ThemeProvider>
   );
 }
